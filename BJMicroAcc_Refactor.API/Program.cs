a1ce4ef6a7468bfcc5d3907747af50ab1b84263c
harsh soni <harshsoni6011@gmail.com>
2025-04-27 17:59:08 +0530

    fix: fix bug and added getbyid endpoint to api

--- BJMicroAcc_Refactor.API/Program.cs
+++ BJMicroAcc_Refactor.API/Program.cs
@@ -12,8 +12,16 @@ builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// builder.Services.AddSwaggerGen();
 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "BJMicro Accounts API",
+        Version = "v1"
+    });
+});
 
 // Register DbContext
 builder.Services.AddDbContext<MicroAccountsContext>(options =>
@@ -28,10 +36,20 @@ builder.Services.AddScoped<IDailyRateService, DailyRateService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "BJMicro Accounts API V1");
+    });
+    //app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
--- BJMicro_Accounts_Refactor.Utils/obj/Debug/net8.0/BJMicro_Accounts_Refactor.Utils.AssemblyInfo.cs
+++ BJMicro_Accounts_Refactor.Utils/obj/Debug/net8.0/BJMicro_Accounts_Refactor.Utils.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BJMicro_Accounts_Refactor.Utils")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c73f29c92fa3b43e4965aee020c12fe747728938")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deedeeb85743a1d27949c4d72d70948e15ffd1df")]
 [assembly: System.Reflection.AssemblyProductAttribute("BJMicro_Accounts_Refactor.Utils")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BJMicro_Accounts_Refactor.Utils")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
